a13ad3fa0b304b0d49d8f035e8bf482f3dc62941
nik-vitaly <nik.vitaly@mail.ru>
2016-10-17 14:14:52 +0300

    mock of data source

---
+++ eQueue/DataSource.swift
@@ -0,0 +1,13 @@
+//
+//  DataSource.swift
+//  eQueue
+//
+//  Created by Виталий Никоноров on 17.10.16.
+//  Copyright © 2016 Vitaly Nikonorov. All rights reserved.
+//
+
+import Foundation
+
+class DataSource {
+    var data = "[{\"qid\": 1, \"name\": \"my Queue\", \"description\": \"Description\", \"users_quantity\": 5, \"address\": \"address\", \"wait_time\": 2, \"in_front\": 1, \"number\": 2, \"coords\": \"\"}, {\"qid\": 3, \"name\": \"my Queue2\", \"description\": \"Description2\", \"users_quantity\": 52, \"address\": \"address2\", \"wait_time\": 22, \"in_front\": 12, \"number\": 22, \"coords\": \"\"}]"
+}
--- eQueue/ViewController.swift
+++ eQueue/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    };
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "QueueCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
