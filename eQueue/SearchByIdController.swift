cac856311c4672614251874356bfa9989e9aacbb
nik-vitaly <nik.vitaly@mail.ru>
2016-10-24 16:32:23 +0300

    add search by id screen

---
+++ eQueue/SearchByIdController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchByIdController.swift
+//  eQueue
+//
+//  Created by Виталий Никоноров on 24.10.16.
+//  Copyright © 2016 Vitaly Nikonorov. All rights reserved.
+//
+
+import UIKit
+
+class SearchByIdController : ViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showQueueSegue"{
+            let dataSource: DataSource = DataSource()
+            let destinationVC = segue.destination as? QueueScreenController
+            destinationVC?.queue = dataSource.getMyQueues()[0]
+        }
+    }
+    
+}
