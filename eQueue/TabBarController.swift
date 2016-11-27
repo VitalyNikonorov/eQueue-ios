935d79f67ae209065d3aade6f7e7955be877c125
VitalyNikonorov <nik.vitaly@mail.ru>
2016-11-27 23:27:16 +0300

    fix my number on ticket queue screen and chahnge autoselected item in tabbar

---
+++ eQueue/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  eQueue
+//
+//  Created by Виталий Никоноров on 27.11.16.
+//  Copyright © 2016 Vitaly Nikonorov. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        self.selectedIndex = 2
+    }
+    
+}
