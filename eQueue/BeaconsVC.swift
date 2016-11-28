eb9cb2730754e9e5fbc5a48e54fd586f0c245aae
VitalyNikonorov <nik.vitaly@mail.ru>
2016-11-28 14:34:52 +0300

    working on beacons view controller

--- eQueue/BeaconsVC.swift
+++ eQueue/BeaconsVC.swift
@@ -12,6 +12,7 @@ class BeaconsVC: UIViewController, BeaconScannerDelegate {
     
 
     var beaconScanner: BeaconScanner!
+    let URL_PATTERN = "http://equeue/"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,16 +31,35 @@ class BeaconsVC: UIViewController, BeaconScannerDelegate {
     }
     
     func didFindBeacon(_ beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
-        print("FIND: %@", beaconInfo.description)
+        //print("FIND: %@", beaconInfo.description)
     }
     func didLoseBeacon(_ beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
-        print("LOST: %@", beaconInfo.description)
+        //print("LOST: %@", beaconInfo.description)
     }
     func didUpdateBeacon(_ beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
-        print("UPDATE: %@", beaconInfo.description)
+        //print("UPDATE: %@", beaconInfo.description)
     }
     func didObserveURLBeacon(_ beaconScanner: BeaconScanner, URL: Foundation.URL, RSSI: Int) {
         print("URL SEEN: %@, RSSI: %d", URL, RSSI)
+        
+        if (URL.absoluteString.contains(URL_PATTERN)){
+            
+            let id = Int(URL.absoluteString.substring(from: URL_PATTERN.endIndex))
+            
+            //TODO
+            
+            guard let vc = UIStoryboard(name:"Main", bundle:nil).instantiateViewController(withIdentifier: "QueueController") as? QueueScreenController else {
+                print("Could not instantiate view controller with identifier of type SecondViewController")
+                return
+            }
+            
+            vc.qid = Int(id!)
+            DispatchQueue.main.async {
+                self.navigationController?.pushViewController(vc, animated:true)
+            }
+        }
+
+        
     }
 
     
